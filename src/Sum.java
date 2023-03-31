16c3fb738e5b8047f155e92fb8f072fa1f0d6779
Thimira <pathiranathimea@gmail.com>
2023-03-31 19:39:07 +0100

    MethodOverloadind

---
+++ src/Sum.java
@@ -0,0 +1,14 @@
+public class Sum {
+    void sum(int a,long b){
+        System.out.println(a+b);
+    }
+    void sum(int c,int d){
+        System.out.println(c+d);
+    }
+
+    public static void main(String[] args) {
+        Sum s=new Sum();
+        s.sum(77,189769L);
+        s.sum(7,77);
+    }
+}
